9373cbdc676b21df286e96c05123aa41d955f5df
KUNTIMADDI MANIDEEP <manideepk90@gmail.com>
2024-07-01 09:11:37 +0530

    Changed Title

--- user_management/user_service/views.py
+++ user_management/user_service/views.py
@@ -94,20 +94,20 @@ class ModifyUserView(APIView):
         try:
             user = User.objects.get(id=request.user.id)
             email = request.data.get("email")
-            password = request.data.get("password")
-            confirm_password = request.data.get("confirm_password")
+            # password = request.data.get("password")
+            # confirm_password = request.data.get("confirm_password")
 
-            if not email or not password:
+            if not email:
                 return Response(
                     {"error": "Please provide all required fields"},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-            if password != confirm_password:
-                return Response(
-                    {"error": "Passwords do not match"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            
+            # if password != confirm_password:
+            #     return Response(
+            #         {"error": "Passwords do not match"},
+            #         status=status.HTTP_400_BAD_REQUEST,
+            #     )
+
         except User.DoesNotExist:
             return Response(
                 {"error": "User not found."}, status=status.HTTP_404_NOT_FOUND
